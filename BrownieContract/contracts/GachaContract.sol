444c8501b8f7e92d99bde16b40cb896740a7848b
idea2547 <ideapakpaphon@gmail.com>
2021-11-23 23:15:40 +0700

    add

--- BrownieContract/contracts/GachaContract.sol
+++ BrownieContract/contracts/GachaContract.sol
@@ -8,11 +8,19 @@ contract Gacha is VRFConsumerBase, Ownable {
 
     bytes32 internal keyHash;
     uint256 internal fee;
+
+    //Gacha variable
+
+    uint256 itemDropChance[]; //Drop rate of the item
+    uint256 rewardID[]; //reward ID of ERC1155 NFT Token
+
     address NFTContract_Address;
     mapping(bytes32 => address) private s_rollers;
     mapping(address => uint256) private s_results;
     
     mapping(address => uint256)[] public NumberOfItem;
+
+    
     
     uint256 public randomResult;
     uint256 private constant ROLL_IN_PROGRESS = 42;
@@ -47,14 +55,43 @@ contract Gacha is VRFConsumerBase, Ownable {
        NFTContract_Address = _address;
     }
 
+    //Sum of array to calculate item_drop_rate
+    function calcSum(uint256[] arr) public return (uint256 sum) {
+        uint256 sum = 0;
+
+        for(uint256 i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+    }
+
     // function to add NFT address and amount of NFT to Gacha container 
     function AddItemInGacha(address NFTContract, uint256 amount) public onlyOwner {
         // .safeBatchTransferFrom(deployerAddress, playerAddress, [0,1,3,4], [50,100,1,1], "0x0")
+
         
     }
 
 
-    function GachaCaller(address roller) public onlyOwner returns (bytes32 requestId) {
+
+   //function sample 
+
+    function sample(uint256[] arr, bytes32 requestId) public return (uint256 index) {
+        //check result from player address
+        uint256 random = s_results[s_rollers[requestId]]
+
+        uint256 sum = 0; //use sum to find the outcome result
+
+        if(sum > random){
+            break;
+        }
+
+        index++; 
+
+
+    }
+
+
+    function GachaRandomCaller(address roller) public onlyOwner returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK to pay fee");
         require(s_results[roller] == 0, "Already rolled");
         requestId = requestRandomness(keyHash, fee);
@@ -70,8 +107,8 @@ contract Gacha is VRFConsumerBase, Ownable {
      */
 
     
-    function fulfillRandomness(bytes32 requestId, uint256 randomness, uint256 total) internal override {
-        uint256 random_result = (randomness % total) + 1;
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        uint256 random_result = (randomness % calcSum) + 1;
         s_results[s_rollers[requestId]] = random_result;
         emit GachaResult(requestId, randomResult);
     }
